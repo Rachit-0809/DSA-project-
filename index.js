1f8b2338306184dbf7c29211ea3a1d51a3538183
Rachit-0809 <149254218+Rachit-0809@users.noreply.github.com>
2024-06-26 21:06:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function solveKnapsack() {
+    window.location.href = 'Knapsack.html';
+}
+
+function solveCoinChange() {
+    window.location.href = 'coinchange.html';
+}
+
+function generateFibonacci() {
+    window.location.href = 'fibonnaci.html'
+}
+
+function multiplyMatrices() {
+    window.location.href = 'Matrix.html'
+}
+document.getElementById("knapsackBtn").addEventListener("click", solveKnapsack);
+document.getElementById("coinChangeBtn").addEventListener("click", solveCoinChange);
+document.getElementById("fibonacciBtn").addEventListener("click", generateFibonacci);
+document.getElementById("matrixChainBtn").addEventListener("click", multiplyMatrices);
